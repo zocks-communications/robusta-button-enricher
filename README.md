dc393e23a78a34b6508426ee61d49584d64e84c4
adamantal <adam.antal@zocks.io>
2025-10-31 16:51:36 +0100

    feat(): init project, add button_enricher_action

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # robusta-button-enricher
+
+[Robusta](https://github.com/robusta-dev/robusta) is an amazing tool for enriching and enhancing your alerting system. This project is an [external action](https://docs.robusta.dev/master/playbook-reference/defining-playbooks/external-playbook-repositories.html#loading-external-actions) that allows you to enrich your alerts with buttons based on the alert context.
+
+## Requirements
+
+- Robusta v0.29+
+
+## Usage
+
+Example usage:
+
+```yaml
+customPlaybooks:
+- triggers:
+  - on_image_pull_backoff: {}
+  actions:
+  - image_pull_backoff_reporter: {}
+  - button_enricher:
+      button_text: "Navigate to ${namespace}"
+      button_url: "https://example.com/namespace/${namespace}"
+```
+
+It supports templating, with the same set of substitutions as the [template_enricher](https://docs.robusta.dev/master/playbook-reference/actions/event-enrichment.html#template-enricher) action which was recommended as an alternative.
+
+> [!NOTE]
+> As per the current implementation, if the `button_url` is empty, the button will not be shown.
---
+++ button_enricher_playbooks/button_enricher.py
@@ -0,0 +1,40 @@
+from typing import Dict, Any
+from collections import defaultdict
+from robusta.api import action, PrometheusKubernetesAlert, KubernetesResourceEvent, TemplatedButtonParams, Link
+from string import Template
+
+
+def _get_labels(event: KubernetesResourceEvent, default_value: str = "<missing>") -> Dict[str, Any]:
+    labels: Dict[str, Any] = defaultdict(lambda: default_value)
+    if isinstance(event, PrometheusKubernetesAlert):
+        labels.update(event.alert.labels)
+        labels.update(event.alert.annotations)
+        labels.update(vars(event.get_alert_subject()))
+        labels["kind"] = labels["subject_type"].value
+    elif isinstance(event, KubernetesResourceEvent):
+        labels.update(vars(event.get_subject()))
+        labels["kind"] = labels["subject_type"].value
+
+    return labels
+
+@action
+def button_enricher(event: KubernetesResourceEvent, params: TemplatedButtonParams):
+    """
+    Create a button with a templated text and URL.
+    You can inject the k8s subject info and additionally on Prometheus alerts, any of the alert's Prometheus labels.
+
+    Common variables to use are ${name}, ${kind}, ${namespace}, and ${node}
+
+    A variable like ${foo} will be replaced by the value of info/label foo.
+    If it isn't present, then the text will be skipped.
+
+    If the button_url is empty, then the button will not be added.
+
+    Check example for adding a template link.
+
+    """
+    labels = _get_labels(event, default_value="")
+    button_text = Template(params.button_text).safe_substitute(labels)
+    button_url = Template(params.button_url).safe_substitute(labels)
+    if button_url.strip() != "":
+        event.add_link(Link(url=button_url, name=button_text))
